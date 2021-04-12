a9fc74ed2d89f8c48c8c51678382d8df7b756fcb
Carlos Alberto de Oliveira <ca.oliveira0265@gmail.com>
2021-04-11 22:56:03 -0300

    Inserindo dados no BD com Statement

---
+++ loja-virtual-repository/src/TestaInsercao.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaInsercao {
+
+	public static void main(String[] args) throws SQLException {
+		
+		ConnectionFactory criaConexao = new ConnectionFactory();
+		Connection connectionFactory = criaConexao.recuperarConexao();
+		
+		Statement stm = connectionFactory.createStatement();
+		stm.execute("INSERT INTO Produto (nome, descricao) VALUES ('TV SMART','TV SMART TOSHIBA')",
+				Statement.RETURN_GENERATED_KEYS);
+		
+		ResultSet rst = stm.getGeneratedKeys();
+		
+		while(rst.next()) {
+			System.out.println("O ID criado é: " + rst.getInt(1));
+		}
+	}
+
+}
