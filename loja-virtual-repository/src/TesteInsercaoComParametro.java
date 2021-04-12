5bb00ced893426c6fa4a3e5f175351cd831ab58a
Carlos Alberto de Oliveira <ca.oliveira0265@gmail.com>
2021-04-12 15:33:15 -0300

    Inclusão com parametro utilizando PreparedStatement

---
+++ loja-virtual-repository/src/TesteInsercaoComParametro.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TesteInsercaoComParametro {
+
+	public static void main(String[] args) throws SQLException {
+		
+		String nome	= "CELULAR";
+		String descricao = "IPHONE 11";
+		
+		ConnectionFactory criaConexao = new ConnectionFactory();
+		Connection connectionFactory = criaConexao.recuperarConexao();
+			
+		PreparedStatement stm = connectionFactory.prepareStatement("INSERT INTO Produto (nome, descricao) VALUES (?, ?)",
+				Statement.RETURN_GENERATED_KEYS);
+		stm.setString(1, nome);
+		stm.setString(2, descricao);
+		stm.execute();
+			
+		ResultSet rst = stm.getGeneratedKeys();
+		while(rst.next()) {
+			System.out.println("O ID criado é: " + rst.getInt(1));
+		}
+			
+		connectionFactory.close();
+		
+	}
+
+}
